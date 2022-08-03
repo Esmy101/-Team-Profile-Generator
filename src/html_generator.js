150077c5bc26de05c189e7d9636ae47ebba5681d
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-08-03 18:20:03 -0400

    added js that holds html

---
+++ src/html_generator.js
@@ -0,0 +1,25 @@
+function boxes (employees){
+    return (`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>
+            <div class="text-bg-danger p-3 text-center"> 
+                <h1> My Team</h1> 
+            </div>
+        </header>
+
+       
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+           
+         </div>   
+    </body>
+ </html>`)
+}
